c0c526e8fcdd70ba681bd3afbcd35cd9a035ee75
apwheele <apwheele@gmail.com>
2022-04-08 16:50:08 +0100

    R condaenv 2

--- R/conda_env/packs.R
+++ R/conda_env/packs.R
@@ -3,9 +3,11 @@
 # conda environment
 
 library(devtools)
-install_github("apwheele/ptools")
 
-# Specifying specific version url
+# Install specific commit/version from github
+install_github("apwheele/ptools",ref=" 9826241c93e9975804430cb3d838329b86f27fd3")
+
+# Specifying specific version url for cran package (not on conda-forge)
 gh_url <- "https://cran.r-project.org/src/contrib/groundhog_1.5.0.tar.gz"
 install.packages(gh_url,repos=NULL,type="source")
 
